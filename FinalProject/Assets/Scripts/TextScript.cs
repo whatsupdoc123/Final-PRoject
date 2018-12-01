92cb9b12562f9df4797b0df35ca1934ca05c34ee
Rachit Goyal <rachit.goyal@knights.ucf.edu>
2018-12-01 18:38:30 -0500

    Commit

---
+++ FinalProject/Assets/Scripts/TextScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScript : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2);
+    }
+
+
+    void Update()
+    {
+
+    }
+}
